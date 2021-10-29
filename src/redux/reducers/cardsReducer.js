e9206eb321ef2163a13c513b1bf2ce73b4df02a9
Azamat Shogen <baki47hanma@gmail.com>
2021-10-28 20:05:46 -0500

    class refactoring work completed

--- src/redux/reducers/cardsReducer.js
+++ src/redux/reducers/cardsReducer.js
@@ -1,47 +1,18 @@
 import CARD_IMAGES from '../../components/board/cards2'
-import { v4 as uuidv4 } from 'uuid';
-import {
-    GAME_STARTED, SELECT_ROW, MIX_CARDS, HIDDEN, RESET_ROUND,
+import {GAME_STARTED, SELECT_ROW, MIX_CARDS, HIDDEN, RESET_ROUND,
     SET_ROUND, ROW_SELECT, STARTED_DISABLED, RESET_CARDS
 } from "./actionTypes";
 import { selectRowUtils } from "../utils";
-
-
-class Card{
-    constructor(source) {
-        this.source = source
-        this.selected = false
-        this.id = uuidv4()
-    }
-}
-
-
-
-const initDeck = () => {
-    const deckArr = [];
-
-    for(let i = 1; i <= 36; i++){
-        const card = new Card(CARD_IMAGES[`card${i}`])
-        deckArr.push(card)
-    }
-
-    for(let i = deckArr.length-1; i > 0; i--){
-        const j = Math.floor(Math.random() * (i + 1));
-        [deckArr[i], deckArr[j]] = [deckArr[j], deckArr[i]]
-    }
-    return deckArr
-}
-
+import {Deck} from "../utils";
 
 
 const initialState = {
-    cards: initDeck(),
+    cards: new Deck(CARD_IMAGES).deck,
     hidden: true,
     rowSelected: false,
     startedDisabled: false,
     round: 0
 }
-
 
 const cardsReducer = (state = initialState, action) => {
     switch (action.type){
@@ -63,12 +34,11 @@ const cardsReducer = (state = initialState, action) => {
         case RESET_ROUND:
             return {...state, round: 0}
         case RESET_CARDS:
-            const newCard = initDeck();
-            newCard.map(el => { return {...el, selected: false}})
-            return {...state, cards: newCard}
+            const newDeck = new Deck(CARD_IMAGES).deck;
+            newDeck.map(el => { return {...el, selected: false}})
+            return {...state, cards: newDeck}
         default:
             return state
-
     }
 }
 
